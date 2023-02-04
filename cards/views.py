284ff41a81378a53652b975e6533e5048c364b2e
hussamahalshawi <hussamahalshawi@gmail.com>
2023-02-04 17:38:58 +0200

    add models move

--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,10 @@
+import random
 from django.shortcuts import render
 from django.views.generic import (ListView, CreateView,UpdateView,) 
 from django.urls import reverse_lazy
 from .models import Card
+from django.shortcuts import get_object_or_404, redirect
+from .forms import CardCheckForm
 # Create your views here.
 class CardListView(ListView):
     model = Card
@@ -13,11 +16,28 @@ class CardCreateView(CreateView):
 
 class CardUpdateView(CardCreateView, UpdateView):
     success_url = reverse_lazy("card-list")
+# class BoxView(CardListView):
+#     template_name = "cards/box.html"
+#     def get_queryset(self):
+#         return Card.objects.filter(box=self.kwargs["box_num"])
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["box_number"] = self.kwargs["box_num"]
+#         return context
 class BoxView(CardListView):
     template_name = "cards/box.html"
+    form_class = CardCheckForm
     def get_queryset(self):
         return Card.objects.filter(box=self.kwargs["box_num"])
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
         return context
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+        return redirect(request.META.get("HTTP_REFERER"))
